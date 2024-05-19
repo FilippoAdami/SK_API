77a5b25816221129da733c18822494e429f7fb99
FilippoAdami <elpippez2@gmail.com>
2024-05-19 18:45:57 +0200

    compilation changes

--- obj/Debug/net7.0/SK_API.AssemblyInfo.cs
+++ obj/Debug/net7.0/SK_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SK_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1f461ad298dd6fdf3381abd422403e4c79b266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+400c30526a1037ac9ac2590380427f240a0b9238")]
 [assembly: System.Reflection.AssemblyProductAttribute("SK_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SK_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
